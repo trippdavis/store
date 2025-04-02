3320aacb69b13b91fe81710f91875693869b74fb
Tripp Davis <tripp@Tripp-Macbook.local>
2025-04-02 12:42:34 -0700

    Handle unauthenticated users
    Allow unauthenticated users to access products index and show pages
    but hide product editing, creating, and destroying. Also add Login
    link to nav bar.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
+  allow_unauthenticated_access only: %i[ index show ]
+
   def index
     @products = Product.all
   end
